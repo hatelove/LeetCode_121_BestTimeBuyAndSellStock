8d25d720e7ec4f2fd143540680b49475ca7ecf8a
hatelove <jay.hatelove@gmail.com>
2017-03-15 21:47:43 +0800

    add the first test case: prices is {5,4} , max profit should be 0

---
+++ LeetCode_121_BestTimeBuyAndSellStock/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_121_BestTimeBuyAndSellStock
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void prices_is_5_4_maxProfit_should_be_0()
+        {
+            var prices = new int[] { 5, 4 };
+            Assert.AreEqual(0, new Solution().MaxProfit(prices));
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
